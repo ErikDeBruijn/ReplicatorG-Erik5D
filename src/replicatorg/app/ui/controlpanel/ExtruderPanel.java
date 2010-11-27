67e3bb8dc7da867f020312c83bc2ed60c79a2686
Erik de Bruijn <reprap@erikdebruijn.nl>
2010-11-27 21:06:25 +0100

    FIX: Pre-seeds the extruder PWM and RPM as configured in machines.xml

--- src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
+++ src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
@@ -162,6 +162,10 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 				field.setName("motor-speed-pwm");
 				field.addFocusListener(this);
 				field.setActionCommand("handleTextfield");
+				field.setText(Integer.toString(t.getMotorSpeedPWM()));
+				try {
+					driver.setMotorSpeedPWM(Integer.parseInt(field.getText()));
+				} catch (Exception e) {} // ignore parse errors.
 				field.addActionListener(this);
 
 				add(label);
@@ -184,6 +188,10 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 				field.setName("motor-speed");
 				field.addFocusListener(this);
 				field.setActionCommand("handleTextfield");
+				field.setText(Double.toString(t.getMotorSpeedRPM()));
+				try {
+				driver.setMotorRPM(Double.parseDouble(field.getText()));
+				} catch (Exception e) {} // ignore parse errors.
 				field.addActionListener(this);
 
 				add(label);
